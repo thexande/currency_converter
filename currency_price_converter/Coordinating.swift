31610d2295d7215f32f06c3978b6c05603b162eb
Alexander Murphy <amurphy345@gmail.com>
2019-09-14 10:59:33 -0600

    adding root coordinator and coordinating protocol

---
+++ currency_price_converter/Coordinating.swift
@@ -0,0 +1,21 @@
+/// Coordinator objects which can launch conform to this protocol.
+/// Other future methods in this protocol could support launching as a modal
+/// or onto a `UINavigationController` stack.
+protocol Launchable {
+    
+    /// Launch from a `UIWindow`, typically at app launch
+    ///
+    /// - Parameter window: a given window to launch into
+    func launch(in window: UIWindow?)
+}
+
+protocol Coordinating: Launchable {
+    var root: UINavigationController { get }
+}
+
+extension Coordinating {
+    func launch(in window: UIWindow?) {
+        window?.rootViewController = root
+        window?.makeKeyAndVisible()
+    }
+}
